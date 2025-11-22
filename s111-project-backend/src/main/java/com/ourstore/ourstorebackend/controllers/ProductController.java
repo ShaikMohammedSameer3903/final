c5fa78f69090ffaf19df429c9aafebbdf27b60ca
ShaikMohammedSameer3903 <2300030621@kluniversity.in>
2025-11-22 23:28:40 +0530

    fir

--- s111-project-backend/src/main/java/com/ourstore/ourstorebackend/controllers/ProductController.java
+++ s111-project-backend/src/main/java/com/ourstore/ourstorebackend/controllers/ProductController.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8082", "*"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8082"})
 public class ProductController {
 
     @Autowired
